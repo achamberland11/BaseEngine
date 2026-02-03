ba06f007d28bc3b0a6c284f7e43fd9f590e3a67b
Virgile <112096529+achamberland11@users.noreply.github.com>
2026-02-03 04:49:35 -0500

    jvais me gunner

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,7 @@
 /*
-  Copyright (C) 1997-2026 Sam Lantinga <slouken@libsdl.org>
-
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any damages
-  arising from the use of this software.
-
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely.
-*/
 #include "Core/GameEngine.h"
 
-/*/* This function runs once at startup. #1#
+/* This function runs once at startup. #1#
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
     CGameEngine::Instance().Init();
